57b1d41fb0d7360c63bf75ffc5112dadcce86d83
Marek Vondra <marek.vondra@t-systems.com>
2023-08-01 16:47:28 +0200

    init commit

---
+++ src/main/java/com/maravondra/mq_communication/MqCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.maravondra.mq_communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqCommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqCommunicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maravondra/mq_communication/MqCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maravondra.mq_communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
